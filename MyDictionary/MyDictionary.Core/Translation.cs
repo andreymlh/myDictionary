1bfaf3d1bb88767cc2fc243ef3479ae35da686e0
Daniil64bit <Daniil64bit.r@gmail.com>
2024-07-10 20:54:43 +0300

    init

---
+++ MyDictionary/MyDictionary.Core/Translation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary.Core
+{
+    /// <summary>
+    /// Класс перевода. 
+    /// Содержит два слова - одно, которое нужно перевести, 
+    /// второе, которое является переводом.
+    /// </summary>
+    public class Translation
+    {
+        public Translation(int id, string firstWord = "", string secondWord = "")
+        {
+            Id = id;
+            FirstWord = firstWord;
+            SecondWord = secondWord;
+        }
+
+        /// <summary>
+        /// Уникальный идентификатор
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Слово для перевода
+        /// </summary>
+        public string FirstWord { get; set; }
+
+        /// <summary>
+        /// Перевод
+        /// </summary>
+        public string SecondWord { get; set; }
+
+        public override string ToString()
+        {
+            return Id + "|" + FirstWord + " - " + SecondWord;
+        }
+    }
+}
---
+++ MyDictionary/MyDictionary.Core/TranslationDataSource.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary.Core
+{
+    /// <summary>
+    /// Класс для доступа к данным о фильмах
+    /// </summary>
+    public class TranslationDataSource
+    {
+        /// <summary>
+        /// Относительный путь к файлу, где хранятся данные
+        /// </summary>
+        private readonly string path = ".\\data.json";
+
+        /// <summary>
+        /// Метод чтения данных в формате JSON 
+        /// и их десериализация
+        /// </summary>
+        /// <returns></returns>
+        public List<Translation> Get()
+        {
+            if (File.Exists(path))
+            {
+                string data = File.ReadAllText(path);
+                return DataSerializer.Deserialize<List<Translation>>(data);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Метод записи данных в формате JSON в файл
+        /// </summary>
+        /// <returns></returns>
+        public void Write(List<Translation> data)
+        {
+            File.WriteAllText(path, DataSerializer.Serialize(data));
+        }
+    }
+}
