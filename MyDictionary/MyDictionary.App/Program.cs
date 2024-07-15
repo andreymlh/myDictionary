fe3407bb6f1e50c178daca3dbf55afd1c3b0cf1d
a.mikhailov <michailov@e-krit.ru>
2024-07-15 10:20:03 +0300

    first commit

---
+++ MyDictionary/MyDictionary.App/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyDictionary.Core;
+//Инициализация списка объектами. Конструктор можно вызывать по разному
+List<Translation> translations = new List<Translation>()
+{
+    new Translation(0,"apple","яблоко"),
+    new Translation(1, "bear", "медведь"),
+    new Translation(2, "cat", "кошка"),
+};
+
+TranslationDataSource translationDataSource = new TranslationDataSource();
+//Записываем список в файл
+translationDataSource.Write(translations);
+//Считываем список из файла и выводим на экран
+Console.WriteLine(string.Join("\n", translationDataSource.Get()));
